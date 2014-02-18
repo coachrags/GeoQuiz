d2e130ce7ce8b955f9dc224528f13174e06e2fd8
coachrags <ragsdale@harding.edu>
2014-02-18 13:40:21 -0600

    Added code to include an ActionBar object and protected it against being run in Android versions Gingerbread and before.

--- GeoQuiz/src/com/example/geoquiz/QuizActivity.java
+++ GeoQuiz/src/com/example/geoquiz/QuizActivity.java
@@ -1,7 +1,10 @@
 package com.example.geoquiz;
 
+import android.annotation.TargetApi;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -66,12 +69,19 @@ public class QuizActivity extends Activity {
 				
 		Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();		
 	}
-
+	
+	@TargetApi(11)
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
+        
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
+        {
+        	ActionBar actionBar = getActionBar();
+        	actionBar.setSubtitle("Bodies of Water");
+        }
         
         mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
